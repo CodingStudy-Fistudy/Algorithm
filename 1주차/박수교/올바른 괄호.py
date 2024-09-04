4def1a4d16b107ae647c1f515c1aab82365029a5
sujji <83086675+psg759@users.noreply.github.com>
2024-09-04 10:33:15 +0900

    Create 올바른 괄호.py

---
+++ 1주차/박수교/올바른 괄호.py
@@ -0,0 +1,12 @@
+def solution(s):
+    tmp = []
+    if '(' not in s or s.count('(') < s.count(')'):
+        return False
+    
+    for i in s:
+        if i == '(':
+            tmp.append(i)
+        elif tmp and i == ')':
+            tmp.pop()
+    
+    return True if not tmp else False
