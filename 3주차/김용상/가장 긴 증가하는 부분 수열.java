42a3be3544b8f9d95253762991cd07a713ade85d
YongSang Kim <109174778+YongNyeo@users.noreply.github.com>
2024-09-22 00:47:47 +0900

    update

--- 3주차/김용상/가장 긴 증가하는 부분 수열.java
+++ 3주차/김용상/가장 긴 증가하는 부분 수열.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class B11053 {
+public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
