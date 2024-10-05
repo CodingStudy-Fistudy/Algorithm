e7486fe177e5dd5396a7d456f543b1686a6e6623
raeunlee <raeun19@naver.com>
2024-10-05 17:10:19 +0900

    Create 네트워크.py

---
+++ 5주차/이라은/네트워크.py
@@ -0,0 +1,39 @@
+from collections import deque
+
+def solution(n, computers):
+    
+    graph = [[] for _ in range(n)]
+    
+    for i in range(n):
+        for j in range(len(computers[i])):
+            if computers[i][j] == 1 and i != j: # i, j 가 연결되어 있음
+                graph[i].append(j)
+    
+    # print(graph)
+    
+    # 전체 다 방문전으로 처리
+    visited = [False] * n
+    
+    q = deque()
+
+    def bfs(start):
+        q.append(start)
+        visited[start] = True
+        while q:
+            now = q.popleft()
+            for nxt in graph[now]:
+                if not visited[nxt]:
+                    visited[nxt] = True
+                    q.append(nxt)
+        
+    count = 0
+    
+    for i in range(n):
+        if not visited[i]:
+            bfs(i)
+            count += 1
+   
+    print(count)
+    return count
+            
+        
