c123934f2f076de9128be30a6b5fb6440462670d
lnukl <64108487+lnukl@users.noreply.github.com>
2024-10-21 09:13:51 +0900

    Create ab.cpp

---
+++ 6주차/송성욱/ab.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    
+    
+    int a, b;
+    cin >> a >> b;
+
+    int result = 1;
+    while (true)
+    {
+        if (a == b)
+            break;
+        else if (a > b)
+        {
+            result = -1;
+            break;
+        }
+
+        if (b % 2 == 0)
+            b /= 2;
+        else if (b % 10 == 1)
+            b = (b - 1) / 10;    
+        else
+        {
+            result = -1;
+            break;
+        }
+        result++;
+    }
+
+    cout << result;
+
+    return 0;
+}
